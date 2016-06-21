1e1333f3f526c09a3e120fa1f128f6adab4af201
Eduardo Moreira <moreira.edu@hotmail.com>
2016-06-21 18:56:57 -0300

    Add interface to system

---
+++ src/interface/Transito.java
@@ -0,0 +1,32 @@
+package interface;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
+
+public class Transito extends JFrame{
+
+	private JPanel panel;
+
+	public Transito(){
+		super();
+		
+		this.initialize();
+		this.setVisible(true);
+
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setBounds(100, 100, 300, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+	}
+}
